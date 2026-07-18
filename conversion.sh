d178c34d1316bad272de7872db5a792a2fae3788
vijay <vijaybehara0@gmail.com>
2026-07-18 22:22:34 +0530

    saved vijay&pranaya

---
+++ conversion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "vijay:: hello pranaya"
+echo "pranaya:: hello vijay"
+echo "vijay:: how are you"
+echo "pranaya:: am fine vijay what about you"
+echo "vijay:: i am doing well pranaya & i want to tell you that is I LOVE YOU"
+echo "pranaya:: i am SORRY vijay"
