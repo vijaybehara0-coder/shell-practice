1a79be6cf663478602ef58f1b91859edcbd419b9
vijay <vijaybehara0@gmail.com>
2026-07-18 22:41:36 +0530

    added variable script

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+a=vijay
+b=pranaya
+echo "$a:: hello $b"
+echo "$b:: hello $a"
+echo "$a:: how are you"
+echo "$b:: am fine $a what about you"
+echo "$a:: i am doing well $b & i want to tell you that is I LOVE YOU"
+echo "$b:: i am SORRY $a"
