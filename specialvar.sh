6e0f4f219fac55a0d1f3d4e538f5d95eabb31fbe
vijay <vijaybehara0@gmail.com>
2026-07-19 10:44:04 +0530

    special

---
+++ specialvar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "The name of the script is $0"
+echo "The first argument is $1"
+echo "The second argument is $2"    
+echo "The third argument is $3"
+echo "The fourth argument is $4"
+echo "The fifth argument is $5"
+echo "The sixth argument is $6"
